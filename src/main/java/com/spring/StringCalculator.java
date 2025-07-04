24040015d3955e34c5c5d6ba7a7f312d1070335c
Nirav0804 <niravpatel0804@gmail.com>
2025-07-04 21:05:58 +0530

    [Added Test 1.1] Added Test for Empty String (Failed).

--- src/main/java/com/spring/StringCalculator.java
+++ src/main/java/com/spring/StringCalculator.java
@@ -2,4 +2,7 @@ package com.spring;
 
 public class StringCalculator {
 
+    public int add(String input){
+        return -1;
+    }
 }
--- src/test/java/com/spring/SpringCalculatorTest.java
+++ src/test/java/com/spring/SpringCalculatorTest.java
@@ -1,5 +1,21 @@
 package com.spring;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class SpringCalculatorTest{
 
+    public StringCalculator stringCalculator;
+
+    @Before
+    public void setUp() { // Call this method before any @Test annotated method is called.
+        stringCalculator = new StringCalculator();
+    }
+
+    @Test
+    public void inputIsEmptyString() {
+        assertEquals(0,stringCalculator.add(""));
+    }
 }
