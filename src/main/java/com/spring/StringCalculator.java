34dc0595109d97ccb25497d65f78f754a0b826c5
Nirav0804 <niravpatel0804@gmail.com>
2025-07-04 23:08:28 +0530

    [Passed and Refactored Test 4.0] Added logic for adding user defined delimiter and refactored the code.

--- src/main/java/com/spring/StringCalculator.java
+++ src/main/java/com/spring/StringCalculator.java
@@ -2,17 +2,36 @@ package com.spring;
 
 public class StringCalculator {
 
-    public int add(String input){
-        String delimiters = "[,\\n]";// Default delimiters
-        if(input.isEmpty()){ // Handling empty String.
-            return 0;
-        }
-        int sum = 0;
-        String[] numbers= input.split(delimiters);// Split the numbers with delimiters.
+    public String addNewDelimiter(String newDelimiter, String delimiters) {
+        String updatedDelimiters = delimiters.substring(0, delimiters.length() - 1); // get the substring that do not have "]"
+        updatedDelimiters += newDelimiter + "]"; // add newDelimiter and "]"
+        return updatedDelimiters;
+    }
 
-        for(int i=0; i<numbers.length; i++){
-            sum += Integer.parseInt(numbers[i]);
+    public int calculateSum(String[] numbers) {
+        int sum = 0;
+        for (String number : numbers) {
+            sum += Integer.parseInt(number);
         }
         return sum;
+    }
+
+    public int add(String input) {
+        String delimiters = "[,\\n]";  // Default delimiters
+
+        if (input.isEmpty()) {
+            return 0;
+        }
+
+        if (input.startsWith("//")) {
+            int delimiterStartIndex = 2; // "//" length
+            int delimiterEndIndex = input.indexOf("\n"); // get the index of first "\n"
+            String newDelimiter = input.substring(delimiterStartIndex, delimiterEndIndex);// get new delimiter
+            input = input.substring(delimiterEndIndex + 1); // get new substring having all numbers and delimiters
+            delimiters = addNewDelimiter(newDelimiter, delimiters);
+        }
+
+        String[] numbers = input.split(delimiters);
+        return calculateSum(numbers);
     }
 }
